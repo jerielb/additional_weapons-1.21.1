206bb683e80f47a0ccd3384aef633e3e53052f18
jerielB <jerielbongco98@gmail.com>
2025-06-08 22:13:40 +1000

    Initial commit

---
+++ src/main/java/com/jerielb/additional_weapons/AdditionalWeaponsClient.java
@@ -0,0 +1,10 @@
+package com.jerielb.additional_weapons;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AdditionalWeaponsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		
+	}
+}
